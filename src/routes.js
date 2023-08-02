718c0c408284eb8896caab48464ecf46fda73539
israelsilva282 <israelcostas@hotmail.com>
2023-08-02 16:05:37 -0300

    criado página de filmes salvos

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Error from "./Error"
+import Favoritos from "./Favoritos"
 import Filme from "./Filme"
 import Home from "./Home"
 import Header from "./components/Header"
@@ -11,6 +12,7 @@ function RoutesApp() {
             <Routes>
                 <Route path="/" element={<Home></Home>} />
                 <Route path="/filme/:id" element={<Filme></Filme>} />
+                <Route path="/favoritos" element={<Favoritos></Favoritos>}></Route>
 
                 <Route path="*" element={<Error></Error>}></Route>
             </Routes>

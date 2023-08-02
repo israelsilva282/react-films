0539cd52d191dbcbeae6610fb0ada3519b12dc3c
israelsilva282 <israelcostas@hotmail.com>
2023-08-02 13:56:24 -0300

    Adicionado chamada à api na pagina home

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import api from '../services/api';
 
 function Home() {
+
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+        async function loadFilmes() {
+            const response = await api.get("movie/now_playing", {
+                params: {
+                    api_key: "85b4aa43936f2e42349897e7d5a612be",
+                    language: "pt-BR",
+                    page: 1,
+                }
+            })
+
+            console.log(response.data)
+        }
+
+        loadFilmes();
+    }, [])
+
+
     return (
         <div>Home</div>
     )

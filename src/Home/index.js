900cf305030d03aa23d1f14370426b224f347ea5
israelsilva282 <israelcostas@hotmail.com>
2023-08-02 14:50:31 -0300

    finalizando home

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../services/api';
+import './home.css';
 
 function Home() {
 
@@ -15,7 +17,7 @@ function Home() {
                 }
             })
 
-            console.log(response.data)
+            setFilmes(response.data.results.slice(0, 10))
         }
 
         loadFilmes();
@@ -23,7 +25,19 @@ function Home() {
 
 
     return (
-        <div>Home</div>
+        <div className='container'>
+            <div className='lista-filmes'>
+                {filmes.map((filme) => {
+                    return (
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
+        </div>
     )
 }
 
